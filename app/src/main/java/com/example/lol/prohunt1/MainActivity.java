b35366bc0ea6ef6ee2c5b5796c8f977ace4c5e83
Dewesh Rauniyar <devil.dewzone@gmail.com>
2016-06-24 23:50:45 +0545

    Few classes removed and few code changes...pie completed

--- app/src/main/java/com/example/lol/prohunt1/MainActivity.java
+++ app/src/main/java/com/example/lol/prohunt1/MainActivity.java
@@ -1,13 +1,148 @@
 package com.example.lol.prohunt1;
+import java.util.ArrayList;
 
-import android.support.v7.app.AppCompatActivity;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendPosition;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private RelativeLayout mainLayout;
+    private PieChart mChart;
+    //ViewGroup params;
+    //martket shares
+    private float[] yData = { 5, 10, 15, 30, 40 };
+    private String[] xData = { "IT", "Mechanical", "Art", "Medical", "Bank" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        mChart = new PieChart(this);
+        // add pie chart to main layout
+        mainLayout.addView(mChart);
+        mainLayout.setBackgroundColor(Color.parseColor("#55656C"));
+
+        // configure pie chart
+        mChart.setUsePercentValues(true);
+        mChart.setDescription("Market Share");
+
+        //layout of the chart
+        ViewGroup.LayoutParams params = mChart.getLayoutParams();
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+
+
+        // enable hole and configure
+        mChart.setDrawHoleEnabled(true);
+        mChart.setHoleColor(Color.BLUE);
+        //mChart.setHoleColor(true);
+        mChart.setHoleRadius(7);
+        mChart.setTransparentCircleRadius(10);
+
+        // enable rotation of the chart by touch
+        mChart.setRotationAngle(0);
+        mChart.setRotationEnabled(true);
+
+        // set a chart value selected listener
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+
+            @Override
+            public void onValueSelected(Entry entry, int dataSetIndex, Highlight highlight) {
+                // display msg when value selected
+                if (entry == null)
+                    return;
+
+                Toast.makeText(MainActivity.this,
+                        xData[entry.getXIndex()] + " = " + entry.getVal() + "%", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        // add data
+        addData();
+
+        // customize legends
+        Legend l = mChart.getLegend();
+        l.setPosition(LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
     }
+
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < yData.length; i++)
+            yVals1.add(new Entry(yData[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xData.length; i++)
+            xVals.add(xData[i]);
+
+        // create pie data set
+        PieDataSet dataSet = new PieDataSet(yVals1, "Market Share");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        // add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        // instantiate pie data object now
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        //data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+
+        // update pie chart
+        mChart.invalidate();
+    }
+
 }
--- app/src/main/java/com/example/lol/prohunt1/PieChartActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.lol.prohunt1;
-
-/**
- * Created by DevilDewzone on 6/24/2016.
- */
-public class PieChartActivity {
-}
