93ef1a27d0b0afd9302aff2b2d61777e29f45076
Dewesh Rauniyar <devil.dewzone@gmail.com>
2016-06-25 09:43:32 +0545

    few modified

--- app/src/main/java/com/example/lol/prohunt1/MainActivity.java
+++ app/src/main/java/com/example/lol/prohunt1/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.lol.prohunt1;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -11,6 +21,12 @@ import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
 
 import android.app.Activity;
@@ -18,24 +34,33 @@ import android.graphics.Color;
 import android.os.Bundle;
 //import android.view.Menu;
 //import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class MainActivity extends Activity {
 
     private RelativeLayout mainLayout;
     private PieChart mChart;
+    private String category = null;
+    private String jstring;
+    private JsonElement elem;
     //ViewGroup params;
     //martket shares
-    private float[] yData = { 5, 10, 15, 30, 40 };
-    private String[] xData = { "IT", "Mechanical", "Art", "Medical", "Bank" };
+    private float[] yData = {5, 10, 15, 30, 40};
+    private String[] xData = {"IT", "Mechanical", "Art", "Medical", "Bank"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
         mChart = new PieChart(this);
         // add pie chart to main layout
@@ -85,11 +110,64 @@ public class MainActivity extends Activity {
         // add data
         addData();
 
+JsonElement elem = new JsonElement() {
+    @Override
+    public BigDecimal getAsBigDecimal() {
+        return super.getAsBigDecimal();
+    }
+}
+        try {
+            elem = new JsonParser().parse(loadJSONFromAsset());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new GsonBuilder().create();
+            Jobs o = gson.fromJson(elem, Jobs.class);
+            System.out.println(o.getCategory());
+        //List<Jobs> lcs = (List<Jobs>) new Gson().fromJson( jstring , Jobs.class);
+
+        Type collectionType = new TypeToken<Collection<Jobs>>(){}.getType();
+        List<Jobs> lcs = (List<Jobs>) new Gson().fromJson(jstring , Jobs.class);
+        Collection<Jobs> enums = gson.fromJson("output.json", collectionType);
+
+
         // customize legends
         Legend l = mChart.getLegend();
         l.setPosition(LegendPosition.RIGHT_OF_CHART);
         l.setXEntrySpace(7);
         l.setYEntrySpace(5);
+    }
+
+    public String loadJSONFromAsset() throws JSONException {
+        String json = null;
+        try {
+
+            InputStream is = getAssets().open("output.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+
+            int id;
+
+            JSONArray array = new JSONArray(json);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject row = array.getJSONObject(i);
+                id = row.getInt("id");
+                category = row.getString("category");
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 
     private void addData() {
